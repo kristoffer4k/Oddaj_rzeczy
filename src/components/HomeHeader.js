aa5ec8577a9a5b85eadc3649b09f1cef5df06edc
kristoffer4k <krzysztof.korbinski@gmail.com>
2022-09-12 16:55:30 +0200

    header-help

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -8,24 +8,33 @@ function HomeHeader() {
             
             <div className='image' />
             
-            <div className='nav'>
-                <nav className='nav-pages'>
-                    <Link to='/login'>Zaloguj</Link>
-                    <Link to='/register'>Załóż konto</Link>
-                </nav>
+            <div className='content'>
+                
+                <div className='nav'>
+                    <nav className='nav-pages'>
+                        <Link to='/login'>Zaloguj</Link>
+                        <Link to='/register'>Załóż konto</Link>
+                    </nav>
 
-                <nav className='nav-home'>
-                    <Link to='#home-header' smooth>Start</Link>
-                    <Link to='#home-columns' smooth>O co chodzi?</Link>
-                    <Link to='#home-about' smooth>O nas</Link>
-                    <Link to='#home-help' smooth>Fundacje i     organizacje</Link>
-                    <Link to='#home-contact' smooth>Kontakt</   Link>
-                </nav>
-            </div>
+                    <nav className='nav-home'>
+                        <Link to='#home-header' smooth>Start</Link>
+                        <Link to='#home-columns' smooth>O co chodzi?</Link>
+                        <Link to='#home-about' smooth>O nas</Link>
+                        <Link to='#home-help' smooth>Fundacje i     organizacje</Link>
+                        <Link to='#home-contact' smooth>Kontakt</   Link>
+                    </nav>
+                </div>
 
-            
+                <div className='help'>
+                    <h1>Zacznij pomagać!<br></br>Oddaj niechciane rzeczy w zaufane ręce</h1>
+                    <div className='decoration'></div>
+                    <div className='buttons'>
+                        <Link to='/login' className='button'><span>ODDAJ RZECZY</span></Link>
+                        <Link to='/login' className='button'><span>ZORGANIZUJ ZBIÓRKĘ</span></Link>
+                    </div>
+                </div>
 
-
+            </div> 
         </section>
 
         
