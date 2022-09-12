87c811b0f0a3140a191fa47c8deca5322b4f8d01
kristoffer4k <krzysztof.korbinski@gmail.com>
2022-09-12 15:34:48 +0200

    header-image position

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -6,21 +6,24 @@ function HomeHeader() {
     return (
         <section className='header' id='home-header'>
             
-            <nav className='nav-pages'>
-                <Link to='/login'>Zaloguj</Link>
-                <Link to='/register'>Załóż konto</Link>
-            </nav>
+            <div className='image' />
+            
+            <div className='nav'>
+                <nav className='nav-pages'>
+                    <Link to='/login'>Zaloguj</Link>
+                    <Link to='/register'>Załóż konto</Link>
+                </nav>
 
-            <nav className='nav-home'>
-                <Link to='#home-header' smooth>Start</Link>
-                <Link to='#home-columns' smooth>O co chodzi?</Link>
-                <Link to='#home-about' smooth>O nas</Link>
-                <Link to='#home-help' smooth>Fundacje i organizacje</Link>
-                <Link to='#home-contact' smooth>Kontakt</Link>
-            </nav>
-
-            <div className='image'>
+                <nav className='nav-home'>
+                    <Link to='#home-header' smooth>Start</Link>
+                    <Link to='#home-columns' smooth>O co chodzi?</Link>
+                    <Link to='#home-about' smooth>O nas</Link>
+                    <Link to='#home-help' smooth>Fundacje i     organizacje</Link>
+                    <Link to='#home-contact' smooth>Kontakt</   Link>
+                </nav>
             </div>
+
+            
 
 
         </section>
