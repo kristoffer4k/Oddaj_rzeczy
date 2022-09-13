61a033871d5e8d0f1ef395ac028d0a9a1211fc78
kristoffer4k <krzysztof.korbinski@gmail.com>
2022-09-13 13:13:20 +0200

    AboutUs done

--- src/components/HomeAboutUs.js
+++ src/components/HomeAboutUs.js
@@ -1,10 +1,22 @@
-import React from 'react';
 import '../scss/HomeAboutUs.scss';
 
 function HomeAboutUs() {
     return (
         <section className='about' id='home-about'>
+            
+            <div className='description'>
+                <h1>O nas</h1>
+                <div className='decoration' />
+                <div className='text'>
+                    <p>Nori grape silver beet broccoli kombu beet 
+                       greens fava bean potato quandong celery. 
+                       Bunya nuts black-eyed pea prairie turnip 
+                       leek lentil turnip greens parsnip.</p>
+                    <div className='signature' />
+                </div>
+            </div>
 
+            <div className='people' />
         </section>
     )
 }
