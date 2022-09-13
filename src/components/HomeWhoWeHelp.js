0806d078ad0d9424efb7ef1ef01b8d580420479d
kristoffer4k <krzysztof.korbinski@gmail.com>
2022-09-13 15:51:40 +0200

    WhoWeHelp in progress

--- src/components/HomeWhoWeHelp.js
+++ src/components/HomeWhoWeHelp.js
@@ -1,9 +1,48 @@
-import React from 'react';
 import '../scss/HomeWhoWeHelp.scss';
 
 function HomeWhoWeHelp() {
     return (
         <section className='help' id='home-help'>
+            <h1>Komu pomagamy?</h1>
+            <div className='decoration'></div>
+            <div className='menu'>
+                <span>Fundacjom</span>
+                <span>Oragnizacjom pozarządowym</span>
+                <span>Lokalnym zbiórkom</span>
+            </div>
+            <div className='foundation'>
+                <p>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, 
+                    z którymi współpracujemy. Możesz sprawdzić czym się zajmują, 
+                    komu pomagają i czego potrzebują.
+                </p>
+                <div className='first-foundation'>
+                    <div className='left-column'>
+                        <span className='name'>Fundacja “Dbam o Zdrowie”</span>
+                        <span className='target'>Cel i misja: Pomoc osobom 
+                        znajdującym się w trudnej sytuacji życiowej.</span>
+                    </div>
+                    <span className='stuff'>ubrania, jedzenie, sprzęt AGD, meble, zabawki</span>
+                </div>
+
+                <div className='second-foundation'>
+                    <div className='left-column'>
+                        <span className='name'>Fundacja “Dla dzieci”</span>
+                        <span className='target'>Cel i misja: Pomoc dzieciom 
+                        z ubogich rodzin.</span>
+                    </div>
+                    <span className='stuff'>ubrania, meble, zabawki</span>
+                </div>
+
+                <div className='third-foundation'>
+                    <div className='left-column'>
+                        <span className='name'>Fundacja “Bez domu”</span>
+                        <span className='target'>Cel i misja: Pomoc dla 
+                        osób nie posiadających miejsca zamieszkania.</span>
+                    </div>
+                    <span className='stuff'>ubrania, jedzenie, ciepłe koce</span>
+                </div>
+
+            </div>
 
         </section>
     )
