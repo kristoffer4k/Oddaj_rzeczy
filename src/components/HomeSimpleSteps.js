4414166b0932f2401bd23e44d362b857e3f7d80e
kristoffer4k <krzysztof.korbinski@gmail.com>
2022-09-13 12:38:47 +0200

    SimpleSteps done

--- src/components/HomeSimpleSteps.js
+++ src/components/HomeSimpleSteps.js
@@ -8,22 +8,25 @@ function HomeSimpleSteps() {
             <div className='decoration'></div>
             <ul className='steps-columns'>
                 <li className='column first-column'>
-                    <div className='icon icon-first'></div>
+                    <div className='icon first-icon'></div>
                     <span className='tittle'>Wybierz rzeczy</span>
                     <hr className='line'></hr>
                     <p className='text'>ubrania, zabawki, sprzęt i inne</p>
                 </li>
                 <li className='column second-column'>
-                <div className='icon second-first'></div>
+                    <div className='icon second-icon'></div>
                     <span className='tittle'>Spakuj je</span>
                     <hr className='line'></hr>
                     <p className='text'>skorzystaj z worków na śmieci</p>
                 </li>
-                <li className='column third-column'></li>
-                    <span className='tittle'>Zdecyduj komu chcesz pomóc</span>
+                <li className='column third-column'>
+                    <div className='icon third-icon'></div>
+                    <span className='tittle'>Zdecyduj komu<br></br> chcesz pomóc</span>
                     <hr className='line'></hr>
                     <p className='text'>wybierz zaufane miejsce</p>
+                </li>
                 <li className='column fourth-column'>
+                    <div className='icon fourth-icon'></div>
                     <span className='tittle'>Zamów kuriera</span>
                     <hr className='line'></hr>
                     <p className='text'>kurier przyjedzie w dogodnym terminie</p>
